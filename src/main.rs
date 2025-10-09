cdaaa30c8c93b1cf09cd5800ec7d27c680a889f7
YoussefDevPro <youssef.pro.coding@gmail.com>
2025-10-09 08:24:38 +0100

    added commands to control pos rot and fov

--- src/main.rs
+++ src/main.rs
@@ -27,38 +27,61 @@ fn main() -> std::io::Result<()> {
     execute!(stdout, EnterAlternateScreen, Hide)?;
 
     let (width, height) = crossterm::terminal::size()?;
-    let mut canvas = Canvas::new(width as usize, height as usize, Color { r: 0, g: 0, b: 0 });
-    // DO SHIT
-    let blahaj = draw_obj(
-        "source/blahaj_tri.obj".to_string(),
-        [0.0, 0.0, 0.0],
-        3.0,
-        [30, 40, 0],
-        [10.0, 0.0, 0.0],
-        width.into(),
-        height.into(),
-        10.0,
-    );
+
+    // Mutable properties for the object
+    let mut rotation = [20, 0, 0];
+    let mut position = [0.0, 0.0, 0.7];
+    let mut fov = 15.0;
+    let scale = 2.0;
+    let center = [0.0, 0.0, 0.0];
+    let path = "source/blahaj_tri.obj".to_string();
 
     loop {
-        if poll(Duration::from_millis(0))?
-            && matches!(
-                read()?,
-                Event::Key(KeyEvent {
-                    code: KeyCode::Char('q'),
-                    ..
-                })
-            )
-        {
-            break;
+        if poll(Duration::from_millis(16))? {
+            if let Event::Key(KeyEvent { code, .. }) = read()? {
+                match code {
+                    KeyCode::Char('q') => break,
+                    // Position ↓↓↓↓↓↓↓↓↓↓↓↓
+                    KeyCode::Char('w') => position[2] += 0.1,
+                    KeyCode::Char('s') => position[2] -= 0.1,
+                    KeyCode::Char('a') => position[0] -= 0.1,
+                    KeyCode::Char('d') => position[0] += 0.1,
+                    KeyCode::Char('r') => position[1] -= 0.1,
+                    KeyCode::Char('f') => position[1] += 0.1,
+
+                    // Rotation ↓↓↓↓↓↓↓↓↓↓
+                    KeyCode::Up => rotation[0] += 5,
+                    KeyCode::Down => rotation[0] -= 5,
+                    KeyCode::Left => rotation[1] += 5,
+                    KeyCode::Right => rotation[1] -= 5,
+                    KeyCode::Char('e') => rotation[2] += 5,
+                    KeyCode::Char('z') => rotation[2] -= 5,
+
+                    // FOV ↓↓↓↓↓↓
+                    KeyCode::Char('=') | KeyCode::Char('+') => fov += 1.0,
+                    KeyCode::Char('-') => fov -= 1.0,
+                    _ => {}
+                }
+            }
         }
 
+        let mut canvas = Canvas::new(width as usize, height as usize, Color { r: 0, g: 0, b: 0 });
+
+        let blahaj = draw_obj(
+            path.clone(),
+            center,
+            scale,
+            rotation,
+            position,
+            width.into(),
+            height.into(),
+            fov,
+        );
+
         let mut seen = HashSet::new();
-        for (x, y, color) in blahaj.clone() {
-            if x >= 0 && y >= 0 && x < width as i32 && y < height as i32 {
-                if seen.insert((x, y)) {
-                    canvas.set_pixel(x as usize, y as usize, 1, color);
-                }
+        for (x, y, color) in blahaj {
+            if seen.insert((x, y)) {
+                canvas.set_pixel(x as usize, y as usize, 1, color);
             }
         }
         let output = canvas.render();
